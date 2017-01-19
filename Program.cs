30eda64877448b8d066c860d91a0c9e90d8fba19
ksindorf000 <ksindorf000@gmail.com>
2017-01-18 20:17:26 -0500

    Accepts and validates user input. Breaks into amount and currency type

--- Program.cs
+++ Program.cs
@@ -8,8 +8,53 @@ namespace CurrencyConverter
 {
     class Program
     {
+        static double amount;
+        static List<string> currency = new List<string>()
+        {
+            "USD", "EUR", "GBP", "INR", "AUD", "CAD", "ZAR", "NZD", "JPY"
+        };
+        static string convertTo;
+        static string toConvert;
+
+        /*****************************************
+        * Main()
+        *****************************************/
         static void Main(string[] args)
         {
+            GetCurrency();
+        }
+
+        /*****************************************
+        * GetCurrency()
+        *   Accepts and validates user input
+        *   Creates an object of <T> Money
+        *****************************************/
+        public static void GetCurrency()
+        {
+            string userEntry;
+            string digits;
+            bool valid = true;
+
+            currency.ForEach(Console.Write);
+            Console.WriteLine("\n Please enter the amount and the type of currency to convert (1.00 USD): ");
+            userEntry = Console.ReadLine();
+
+            if (!userEntry.Contains("."))
+            {
+                Console.WriteLine("Please be sure that your amount is formatted like so: 0.00");
+            }
+
+            digits = string.Join("", userEntry.Where(char.IsDigit));
+            valid = double.TryParse(digits, out amount);
+
+            convertTo = string.Join("", userEntry.Where(char.IsLetter));
+            
+            if (!currency.Contains(convertTo))
+            {
+                Console.WriteLine($"{convertTo} is not an available currency.");
+            }
+
+            Console.WriteLine(amount + "\n " + convertTo);
         }
     }
 }
